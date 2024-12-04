31cfea5496c3cf9f58c0215346c6c28cc544864c
s-vasu <vasu.suryavamsham@zemosolabs.com>
2024-12-04 06:17:25 +0530

    fixed issues in sonar, added file in .run to exclude the main file in coverage

--- src/main/java/com/zemoso/author_monolithic/controller/AuthorController.java
+++ src/main/java/com/zemoso/author_monolithic/controller/AuthorController.java
@@ -10,8 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
