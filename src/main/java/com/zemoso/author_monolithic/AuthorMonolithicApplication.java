2de2804a9d467b1866616ff26b6e73632e39d303
s-vasu <vasu.suryavamsham@zemosolabs.com>
2024-12-04 06:41:06 +0530

    removed @Generated annotatin as it is not working to exclude from coverage

--- src/main/java/com/zemoso/author_monolithic/AuthorMonolithicApplication.java
+++ src/main/java/com/zemoso/author_monolithic/AuthorMonolithicApplication.java
@@ -3,9 +3,8 @@ package com.zemoso.author_monolithic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.processing.Generated;
 
-@Generated("SonarQube")
+
 @SpringBootApplication
 public class AuthorMonolithicApplication {
 	public static void main(String[] args) {
